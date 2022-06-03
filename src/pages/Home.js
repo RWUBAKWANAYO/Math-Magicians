62c43bc938b655d8f3384def9d55de23cae1c086
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-06-03 06:54:19 +0200

    Create Home, Quotes pages

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home-cont">
+    <h2>Welcome to our page</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+      lobortis, nibh et ullamcorper venenatis, turpis est commodo leo, eget
+      aliquam nisi leo sit amet arcu. Fusce at feugiat ex. Phasellus ullamcorper
+      libero in porta interdum. Maecenas sapien dui, finibus ac quam eget,
+      placerat vestibulum augue. Sed et feugiat ante. Ut ac fringilla est. Sed
+      at nunc purus. Duis non commodo diam. Proin feugiat lacus sed risus
+      sollicitudin, a lacinia erat suscipit. Pellentesque vel ultrices velit.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+      lobortis, nibh et ullamcorper venenatis, turpis est commodo leo, eget
+      aliquam nisi leo sit amet arcu. Fusce at feugiat ex. Phasellus ullamcorper
+      libero in porta interdum. Maecenas sapien dui, finibus ac quam eget,
+      placerat vestibulum augue. Sed et feugiat ante. Ut ac fringilla est. Sed
+      at nunc purus. Duis non commodo diam. Proin feugiat lacus sed risus
+      sollicitudin, a lacinia erat suscipit. Pellentesque vel ultrices velit.
+    </p>
+  </div>
+);
+
+export default Home;
